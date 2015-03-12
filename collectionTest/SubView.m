bb3ba1bb1ee519a3545875395c7889243bcdf9c0
tototti <tototti@gmail.com>
2015-03-12 21:07:16 +0900

    SubViewのinitWithFrame内でsubviewを作成する。

--- collectionTest/SubView.m
+++ collectionTest/SubView.m
@@ -10,10 +10,24 @@
 
 @implementation SubView
 
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.backgroundColor = [UIColor whiteColor];
+        self.subCollectionView = [[SubCollectionView alloc] initWithFrame:self.frame];
+        [self addSubview:self.subCollectionView];
+    }
+    return self;
+}
+
 -(void)layoutSubviews {
-    self.backgroundColor = [UIColor whiteColor];
-    self.subCollectionView = [[SubCollectionView alloc] initWithFrame:self.frame];
-    [self addSubview:self.subCollectionView];
+    [super layoutSubviews];
+
+    // 1. layoutSubviews内でself.subCollectionViewのinitを行っていると、ViewControllerのviewDidLoad内ではself.subCollectionViewが作成されない→当然、subCollectionView.delegateを設定できない。そういうのはinitでやりましょう。
+    // 2. そもそも、layoutSubviewsではlayout調整のみを行うべき
+
+//    self.backgroundColor = [UIColor whiteColor];
+//    self.subCollectionView = [[SubCollectionView alloc] initWithFrame:self.frame];
+//    [self addSubview:self.subCollectionView];
 }
 
 @end
